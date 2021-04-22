760d6f620184ac1dbabffc237f7f12b85b5ac950
Keisuke Tanaka <keinskyrocket@icloud.com>
2021-04-22 13:52:09 +1200

    Convert header, footer and meta into js.

---
+++ scripts/header.js
@@ -0,0 +1,17 @@
+document.write('\
+<header class="bg-secondary">\
+  <div class="inner">\
+    <nav>\
+      <ul>\
+        <li>Keisuke Tanaka</li>\
+        <li>\
+          <ul class="menu">\
+            <li><a href="../index.html">Home</a></li>\
+            <li><a href="2021-04-14_dentity-values and-strength.html">About</a></li>\
+          </ul>\
+        </li>\
+      </ul>\
+    </nav>\
+  </div>\
+</header>\
+');
