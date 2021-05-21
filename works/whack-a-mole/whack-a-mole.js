549624f3abf3f66195911ff2efc62115047a87b8
Keisuke Tanaka <keinskyrocket@icloud.com>
2021-05-21 14:35:33 +1200

    Update whack-a-mole

--- works/whack-a-mole/whack-a-mole.js
+++ works/whack-a-mole/whack-a-mole.js
@@ -6,38 +6,44 @@ console.log("Whack-a-Mole!")
 
 const myBoard = [];
 
-document.getElementsByTagName("table")[0].id = "myTable";  // Pass new ID to <table>
+const myCells = document.querySelectorAll("td");
+const complete = document.querySelector(".complete");
 
-const myCells = document.querySelectorAll("td");  // Assign <img> to all the <td> in <table>
 for (let i = 0; i < myCells.length; i++) {
-  myBoard.push({
-    target: "whack-a-mole/mole.PNG",
-    visibility: true,
-    tag: i
-  });
-  const myImg = document.createElement("img");  // Insert <img>
-  myImg.src = myBoard[i].target;  // myImg.src = "mole.PNG";
-  myImg.width = "100";
+  const myImg = document.createElement("img");
+  myImg.src = "whack-a-mole/mole.PNG"; 
+  myImg.style.visibility = "hidden";
   myCells[i].appendChild(myImg);
-  myCells[i].style.backgroundColor = "#fdf0c1";
-  myCells[i].style.borderRadius = "16px";
-  myCells[i].style.cursor = "pointer";
-  myImg.id = "moleImg";
 }
 
-function randomMole() {
-  const myVisibility = document.querySelectorAll("#moleImg");
-  const pickOne = myBoard[Math.floor(Math.random() * myBoard.length)];  // Pick one object from myBoard
-  var result = pickOne.tag  // Get value of tag property
-  for (let i = 0; i < myCells.length; i++) {
-    if (myBoard[i].tag === pickOne.tag) {
-      myCells[i].style.pointerEvents = "auto";
-      myVisibility[i].style.visibility = "visible";
-    } else {
-      myVisibility[i].style.visibility = "hidden";
-      myCells[i].style.pointerEvents = "none";
-    }
-  }
-  myCells[result].addEventListener("click", function () { randomMole(); }, true);
+function gameStart() {
+  var myShuffle = setInterval(function(){   
+    const pickOne = Math.floor(Math.random() * 25);
+    const myVisibility = document.querySelectorAll("img");
+    myVisibility[pickOne].style.visibility = "visible";
+    var pp = setTimeout(function hideMe() {
+      myVisibility[pickOne].style.visibility = "hidden";
+    }, 1000);
+  }, 1000);
+  
+  myCells.forEach(function(myTarget) {
+    myTarget.addEventListener("click", function(evtHit) {
+      if (evtHit.target.style.visibility === "visible") {
+        clearInterval(myShuffle);
+        complete.classList.add("show-complete");
+      }
+    })
+  });
 }
-randomMole();
+
+const btnStart = document.getElementById("start");
+btnStart.addEventListener("click", function(evtStart) {
+  gameStart();
+  evtStart.target.style.visibility = "hidden";
+});
+
+const btnRestart = document.getElementById("restart");
+btnRestart.addEventListener("click", function() {
+  complete.classList.remove("show-complete");
+  gameStart();
+});
